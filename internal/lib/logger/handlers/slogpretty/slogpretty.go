485361e6a30b82cca89f5a4c997ac0a280490be1
Sergei Hilton <uzoversergey@gmail.com>
2024-02-23 18:33:10 +0500

    fix timestamp bug

--- internal/lib/logger/handlers/slogpretty/slogpretty.go
+++ internal/lib/logger/handlers/slogpretty/slogpretty.go
@@ -68,7 +68,7 @@ func (h *PrettyHandler) Handle(_ context.Context, r slog.Record) error {
 		}
 	}
 
-	timeStr := r.Time.Format("[15:05:05.000]")
+	timeStr := r.Time.Format("[15:04:05.000]")
 	msg := color.CyanString(r.Message)
 
 	h.l.Println(
